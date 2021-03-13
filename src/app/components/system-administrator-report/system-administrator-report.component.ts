109a5059d6afcce3617932f19d7c3af7e89a16d9
praka07 <prakashbtw.007@gmail.com>
2021-03-13 06:57:52 +0530

    report

--- src/app/components/system-administrator-report/system-administrator-report.component.ts
+++ src/app/components/system-administrator-report/system-administrator-report.component.ts
@@ -14,7 +14,7 @@ export class SystemAdministratorReportComponent implements OnInit {
   public context: CanvasRenderingContext2D;
   barChart: Chart;
 
-  constructor(private service: GlobalVolunteerService,private toastr:ToastrService) { }
+  constructor(private service: GlobalVolunteerService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
     this.service.getReport().subscribe(res => {
@@ -30,12 +30,84 @@ export class SystemAdministratorReportComponent implements OnInit {
   }
 
   plotChart(data: any) {
-    let labels = [];
+    let xAxes = [];
     let yAxes = [];
+
+    data.forEach(element => {
+      xAxes.push(element['activityName']);
+
+    });
+    data.forEach(element => {
+      yAxes.push(element['volunteerCount']);
+    })
+    console.log('activityName,xAxes', xAxes);
+    console.log('volunteerCount, yAxes', yAxes);
+    let chartData = {
+      labels: xAxes,
+      datasets: [{
+
+        data: yAxes,
+
+        barThickness: 20,
+
+        backgroundColor: [
+          "#0000CC",
+          "#CC00CC",
+          "#606060",
+          "#3399FF",
+          "#CC0000",
+          "#006600",
+          "#FF8000",
+          "#FF33FF",
+          "#999900"
+        ],
+
+        fill: true
+      }]
+
+    };
 
     if (this.barChart) {
       this.barChart.data.datasets = [];
       this.barChart.reset();
     }
+    this.barChart = new Chart(this.context, {
+      type: 'bar',
+      data: chartData,
+      options: {
+
+        responsive: true,
+        legend: {
+          display: false
+        },
+        scales: {
+          xAxes: [{
+
+            scaleLabel: {
+              display: true,
+              labelString: 'Activity Name',
+              fontStyle: 'bold',
+              fontColor: 'black'
+            },
+            gridLines: {
+              drawOnChartArea: false
+            }
+          }],
+          yAxes: [{
+
+            ticks: { min: 0 },
+            scaleLabel: {
+              display: true,
+              labelString: 'Volunteer Count',
+              fontStyle: 'bold',
+              fontColor: 'black'
+            },
+            gridLines: {
+              drawOnChartArea: false
+            }
+          }],
+        }
+      }
+    });
   }
 }
